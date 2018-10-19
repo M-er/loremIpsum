b25607d8cfc35db975105a18d40b20d3e835087e
M-er <m@tres-erres.com.ar>
2018-10-19 10:54:55 -0300

    Acomodado un poco el admin

---
+++ admin/assets/js/util.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+	console.log("Inicializando ");
+	$("#nav").load("./pages/_navbar.html");
+	$("#side").load("./pages/_sidebar.html");
+	$("#footer").load("./pages/_footer.html");
+});
